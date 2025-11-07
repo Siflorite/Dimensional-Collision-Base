4c67981ed8e011b773e7fdcb574f46d97044f831
Siflorite <monokhrom@163.com>
2025-11-07 15:28:20 +0800

    Added Behavior Tree and Blackboard

--- Source/DCReborn/Private/AI/CAIController.cpp
+++ Source/DCReborn/Private/AI/CAIController.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AI/CAIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 
@@ -14,23 +16,114 @@ ACAIController::ACAIController()
 	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
 	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
 
-	SightConfig->SightRadius = 1000.f;
-	SightConfig->LoseSightRadius = 1200.f;
+	SightConfig->SightRadius = SightRadius;
+	SightConfig->LoseSightRadius = SightRadius + 200.f;
 
 	SightConfig->SetMaxAge(5.f); // 目标走出LoseSightRadius距离后仍然索敌的时间
 
 	SightConfig->PeripheralVisionAngleDegrees = 180.f; // 前向量正负180度
 
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
+	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ACAIController::TargetPerceptionUpdated);
+
+	TargetsInSight.Empty();
 }
 
+// Only executed on server side
 void ACAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
-	SetGenericTeamId(FGenericTeamId(0));
+	// 设置Controller的TeamId
+	SetGenericTeamId(FGenericTeamId(0)); // Temporarily use TeamId = 0 <=> Team01
 
+	// 设置角色（一般是ACCharacter）的TeamId
 	if (IGenericTeamAgentInterface* PawnTeamInterface = Cast<IGenericTeamAgentInterface>(InPawn))
 	{
 		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
 	}
+}
+
+void ACAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (BehaviorTree)
+	{
+		RunBehaviorTree(BehaviorTree);
+	}
+}
+
+// ReSharper disable once CppPassValueParameterByConstReference
+void ACAIController::TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus Stimulus)
+{
+	if (Stimulus.WasSuccessfullySensed()) // 看到新目标
+	{
+		TargetsInSight.AddUnique(TargetActor);
+		if (!GetCurrentTarget()) // 当前无追踪目标，设置目标为看到的Actor
+		{
+			SetCurrentTarget(TargetActor);
+		}
+		// 有追踪目标时保持追踪目标
+	}
+	else // 有玩家离开视野
+	{
+		TargetsInSight.Remove(TargetActor);
+		if (TargetsInSight.IsEmpty())
+		{
+			SetCurrentTarget(nullptr);
+		}
+		else
+		{
+			SetCurrentTarget(FindNearestTargetInSight());
+		}
+	}
+}
+
+UObject* ACAIController::GetCurrentTarget() const
+{
+	if (const UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+	{
+		return BlackboardComponent->GetValueAsObject(TargetBlackboardKeyName);
+	}
+	return nullptr;
+}
+
+void ACAIController::SetCurrentTarget(UObject* NewTarget)
+{
+	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+	{
+		if (NewTarget)
+		{
+			BlackboardComponent->SetValueAsObject(TargetBlackboardKeyName, NewTarget);
+		}
+		else
+		{
+			BlackboardComponent->ClearValue(TargetBlackboardKeyName);
+		}
+	}
+}
+
+UObject* ACAIController::FindNearestTargetInSight() const
+{
+	if (!TargetsInSight.IsEmpty())
+	{
+		if (const APawn* SelfPawn = GetPawn())
+		{
+			const FVector SelfLocation = SelfPawn->GetActorLocation();
+			UObject* NearestTarget = nullptr;
+			float NearestDistance = SightRadius;
+
+			for (AActor* TargetActor : TargetsInSight)
+			{
+				if (!TargetActor) continue;
+
+				if (const float Distance = FVector::Distance(SelfLocation, TargetActor->GetActorLocation()); Distance < NearestDistance)
+				{
+					NearestDistance = Distance;
+					NearestTarget = TargetActor;
+				}
+			}
+			return NearestTarget;
+		}
+	}
+	return nullptr;
 }
--- Source/DCReborn/Private/AI/CAIController.h
+++ Source/DCReborn/Private/AI/CAIController.h
@@ -18,10 +18,31 @@ public:
 	ACAIController();
 
 	virtual void OnPossess(APawn* InPawn) override;
+	virtual void BeginPlay() override;
 private:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
 	class UAIPerceptionComponent* AIPerceptionComponent;
 
 	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
 	class UAISenseConfig_Sight* SightConfig;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Perception")
+	float SightRadius = 1000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behavior")
+	class UBehaviorTree* BehaviorTree;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behavior")
+	FName TargetBlackboardKeyName = "Target";
+	
+	UFUNCTION()
+	void TargetPerceptionUpdated(AActor* TargetActor, struct FAIStimulus Stimulus);
+	
+	UObject* GetCurrentTarget() const;
+	void SetCurrentTarget(UObject* NewTarget);
+
+	UPROPERTY()
+	TArray<AActor*> TargetsInSight;
+
+	UObject* FindNearestTargetInSight() const;
 };
